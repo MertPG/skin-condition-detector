ce290d1b23df1239d93a4242d4993a675426dde3
Mert Çağlayan <mert.caglayan018@gmail.com>
2025-08-06 15:34:24 +0300

    Skin_APP.py güncellendi

--- Skin_APP.py
+++ Skin_APP.py
@@ -1,25 +1,23 @@
 from fastai.vision.all import *
 import gradio as gr
 
-# Modeli yükle
+
 learn = load_learner("M:/Skin_Analyz/skin_model.pkl")
 
 
-# Tahmin fonksiyonu
+
 def classify_skin(img):
     img = PILImage.create(img)
     pred, _, probs = learn.predict(img)
 
-    # Tahmin edilen etiketleri birleştir
+
     predicted_labels = ", ".join(pred)
 
-    # Olasılık sözlüğü
     result = {learn.dls.vocab[i]: float(f"{probs[i]:.2f}") for i in range(len(probs))}
 
     return predicted_labels, result
 
 
-# Gradio arayüzü
 iface = gr.Interface(
     fn=classify_skin,
     inputs=gr.Image(type="pil"),
